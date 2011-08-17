fe59b3faec4523296dd9aabc2718e93f918c696e
Oleg Parashchenko <olpa@uucode.com>
2011-08-17 15:12:44 +0200

    Using a file less in size.

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 export SSAXLIB=`pwd`/support/ssax-sxml
-gsi convert.scm --xml samples_docs/docbook/definitive_guide/en/book5.xml --tex x.tex
+#gsi convert.scm --xml samples_docs/docbook/definitive_guide/en/book5.xml --tex x.tex
+gsi convert.scm --xml samples_docs/docbook/definitive_guide/en/src/ch01.xml --tex x.tex
