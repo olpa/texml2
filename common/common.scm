58f8e29b45483ea36efbd3687d31799e8e4002b4
Oleg Parashchenko <olpa@uucode.com>
2011-08-17 15:14:20 +0200

    No more tag "parm", using "gr".

--- common/common.scm
+++ common/common.scm
@@ -1,7 +1,9 @@
+(define sxslt-id (lambda args args))
+
 (define (common-transform doc)
   (pre-post-order doc `(
       (*default* *preorder* . ,(lambda (tag . rest)
           (display (string-append "*** Unprocessed tag: " (symbol->string tag) "\n") (current-error-port))
-          `(cmd "TODO" (parm ,(symbol->string tag)))))
+          `(cmd "TODO" (gr ,(symbol->string tag)))))
       (*TOP* . ,(lambda (tag . rest) (cons 'texml rest)))
       )))
