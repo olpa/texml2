9b2c34ef40e956c835fc133e83b5764f903d082c
Oleg Parashchenko <olpa@uucode.com>
2011-08-24 13:39:39 +0200

    A function to translate a key.

--- common/lang.scm
+++ common/lang.scm
@@ -24,7 +24,7 @@
                     (list (string-append lang "/"  k) v) translations-table))))
             nset))
 
-;
+; Load several language files, plus the default one
 (define (load-languages llist)
   (let loop ((to-load (cons default-language llist))
              (loaded  '()))
@@ -35,22 +35,14 @@
           (lang-xml-to-table cur-lang (parse-language-file cur-lang)))
         (loop (cdr to-load) (cons cur-lang loaded))))))
 
-; devel
-(define ssax-sxml (getenv "SSAXLIB"))
-(load (string-append ssax-sxml "/libs/gambit/common.scm"))
-(load (string-append ssax-sxml "/libs/gambit/myenv.scm"))
-(load (string-append ssax-sxml "/libs/input-parse.scm"))
-(load (string-append ssax-sxml "/libs/srfi-13-local.scm"))
-(load (string-append ssax-sxml "/libs/look-for-str.scm"))
-(load (string-append ssax-sxml "/ssax/char-encoding.scm"))
-(load (string-append ssax-sxml "/ssax/SSAX-code.scm"))
-(load (string-append ssax-sxml "/ssax/SXML-tree-trans.scm"))
-(load (string-append ssax-sxml "/sxml-tools/xpath-parser.scm"))
-(load (string-append ssax-sxml "/sxml-tools/sxpath.scm"))
-(load (string-append ssax-sxml "/sxml-tools/sxpathlib.scm"))
-(load (string-append ssax-sxml "/sxml-tools/sxpath-ext.scm"))
-(load (string-append ssax-sxml "/sxml-tools/txpath.scm"))
-;(define doc (parse-language-file "ru"))
-;(lang-xml-to-table "ru" doc)
-(load-languages '("ru" "ru" "ru"))
-(pp translations-table (current-output-port))
+; Translate
+(define (gentext lang key)
+  (let* ((lang-key (string-append lang "/" key))
+         (transl   (assoc lang-key translations-table)))
+    (if transl
+      (cadr transl)
+      (begin
+        (cerr "*** No translation for " lang-key #\newline)
+        (if (string=? lang default-language)
+          ""
+          (gentext default-language key))))))
