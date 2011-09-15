0755078fe2daec966af0a0d80ab00bac1e82cc8d
Oleg Parashchenko <olpa@uucode.com>
2011-09-15 15:03:55 +0200

    colspec parsing seems working.

--- common/table.scm
+++ common/table.scm
@@ -30,23 +30,32 @@
 ;Default = IMPLIED (means obtain value from a named style specification if any. If there is no such value, or if the value is "" [the null string] then assume a proportion of "1*".)
 ;The fixed unit values are case insensitive. The standard list of allowed unit values is "pt" (points), "cm" (centimeters), "mm" (millimeters), "pi" (picas), and "in" (inches). The default fixed unit should be interpreted as "pt" if neither a proportion nor a fixed unit is specified.
 
+; correct strings are parsed correctly. For the rest:
+; garbage in -- garbage out.
 (define (parse-colspec-string s)
   (call-with-current-continuation (lambda (func-exit)
-    (let* ((default-result        '(#f #f 1 #f))
-           (chars-value           #f)
-           (chars-units-or-marker #f)
-           (fixed-value           #f)
-           (fixed-units           #f)
-           (percent-value         #f)
-           (proportion-value      #f)
-           (parse-error           (lambda ls
-                                    (apply cerr ls)
-                                    (func-exit default-result)))
-           (reset-value           (lambda ()
-                                    (set! chars-value '())
-                                    (set! chars-unit-or-marker '())))
-           (fix-current-value     (lambda () #t))
+    (let* ((default-result    '(#f #f 1 #f))
+           (chars-value       #f)
+           (chars-units       #f)
+           (fixed-value       #f)
+           (fixed-units       #f)
+           (percent-value     #f)
+           (proportion-value  #f)
+           (parse-error       (lambda ls
+                                (apply cerr ls)
+                                (func-exit default-result)))
+           (reset-value       (lambda ()
+                                (set! chars-value '())
+                                (set! chars-units '())))
+           (fix-current-value (lambda ()
+              (if (not (null? chars-value))
+                (begin
+                  (set! fixed-value (list->string (reverse chars-value)))
+                  (set! fixed-units (if (null? chars-units)
+                          "pt"
+                          (list->string (reverse chars-units))))))))
            )
+      (reset-value)
       (let loop ((rest (string->list s)))
         (if (null? rest)
           (begin
@@ -54,8 +63,31 @@
             (list fixed-value fixed-units proportion-value percent-value))
           (let ((ch (car rest)))
             (cond
-              ((eq? ch #\space) (parse-error "*** space" 22))
-              (else (loop (cdr rest)))))))
-      ))))
+              ((char-numeric? ch)    (set! chars-value (cons ch chars-value)))
+              ((char-lower-case? ch) (set! chars-units (cons ch chars-units)))
+              ((char-upper-case? ch) (set! chars-units
+                                       (cons (char-downcase ch) chars-units)))
+              ((eqv? #\+ ch)         (fix-current-value)
+                                     (reset-value))
+              ((or (eqv? #\. ch) (eqv? #\, ch))
+                      (cond
+                        ((null? chars-value)  (set! chars-value '(#\0 #\.)))
+                        ((memv #\. chars-value) )
+                        (else  (set! chars-value (cons #\. chars-value)))))
+              ((eqv? #\* ch)
+                      (set! proportion-value
+                        (if (null? chars-value)
+                          1
+                          (string->number (list->string
+                                            (reverse chars-value)))))
+                      (reset-value))
+              ((eqv? #\% ch)
+                      (if (not (null? chars-value))
+                        (set! percent-value (string->number
+                                (list->string (reverse chars-value)))))
+                      (reset-value))
+              )
+            (loop (cdr rest))))))
+      )))
 
-(pp (parse-colspec-string "x"))
+;(pp (parse-colspec-string "2 3 C m + 2..5* +  7%%%"))
